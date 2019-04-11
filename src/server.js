a77f19c673fbd3ccd6122047f30628480010e001
José Vitor <josevitor_amorim@hotmail.com>
2019-04-11 19:08:40 -0400

    tentando arrumar o socket

--- src/server.js
+++ src/server.js
@@ -36,4 +36,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
